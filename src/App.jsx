fad343806dbba0f35b2572db56b2deee98278ed2
Dan <begalievvvv@gmail.com>
2024-07-17 18:43:15 -0700

    Changed the component's name

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Random from './components/Random'
+import Dan from './components/Dan'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Random/>
+<Dan/>
     </>
   )
 }
